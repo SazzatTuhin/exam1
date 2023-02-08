6c42a9b672030f9655232f8050f3aa0bf251a12c
SazzatTuhin <sazzattuhin@gmail.com>
2023-02-08 20:15:44 +0600

    exam

---
+++ question.js
@@ -0,0 +1,32 @@
+// Answer 4 :
+
+// const day = "sun";
+// switch (day) {
+//   case "Fri,sat,Mon,wed":
+//     console.log("class day");
+//     break;
+//   default:
+//     console.log("No class today");
+// }
+
+// Answer 1 (i)
+
+// const a = [2, 4, 7, 11, 13, 14, 17, 19, 20];
+// let numbers = [];
+
+// function evenNumbers(even) {
+//   for (i = 2; i < a.length - 1; i++) {
+//     if (i % 2 === 0) numbers.push(i);
+//   }
+//   return numbers;
+// }
+
+// console.log(evenNumbers(numbers));
+
+// Answer :5
+
+const circleFunc = (pai, radious) => {
+  return pai * radious * radious;
+};
+
+console.log(circleFunc(3.1416, 5));
